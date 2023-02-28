8c06c98d8211ee56932d24e6016facf66778c8c7
navi1603 <ukrainec1603@gmail.com>
2023-02-28 14:40:02 +0300

    Создали проект Hello world.

---
+++ Titris/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Titris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
