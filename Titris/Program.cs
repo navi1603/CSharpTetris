6bbc881298b27cc93b425091cd60d8c836fbef38
navi1603 <ukrainec1603@gmail.com>
2023-02-28 15:40:29 +0300

    1

--- Titris/Program.cs
+++ Titris/Program.cs
@@ -12,11 +12,11 @@ namespace Titris
             int x1 = 2;
             int y1 = 3;
             char c1 = '*';
-            Drow(x1, y1, c1);
+            Draw(x1, y1, c1);
 
         }
 
-        static void Drow(int x, int y, char c){
+        static void Draw(int x, int y, char c){
             Console.SetCursorPosition(x, y);
             Console.Write(c);
         }
