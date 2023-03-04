99d4443990cf061bb17faf240a62121b1d93e5fb
navi1603 <ukrainec1603@gmail.com>
2023-03-04 22:27:22 +0300

    Add interface

---
+++ Titris/ConsoleDrawer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Titris
+{
+    internal class ConsoleDrawer : iDrawer
+    {
+        public void DrawPoint(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write('*');
+            Console.SetCursorPosition(0, 0);
+        }
+
+        public void HidePoint(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(' ');
+            Console.SetCursorPosition(0, 0);
+        }
+    }
+}
+
---
+++ Titris/iDrawer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Titris
+{
+    internal interface iDrawer
+    {
+        void DrawPoint(int x, int y);
+        void HidePoint(int x, int y);
+    }
+}
